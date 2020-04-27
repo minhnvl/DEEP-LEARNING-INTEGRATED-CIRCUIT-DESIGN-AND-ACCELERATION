1bcc3248f4909b5f9d45a71a44becf933c8f14c1
minhnvl <56904021+minhnvl@users.noreply.github.com>
2020-04-27 17:05:49 +0800

    Add files via upload

--- Homework 2/CS.v
+++ Homework 2/CS.v
@@ -1,100 +1,64 @@
 `timescale 1ns/10ps
 module CS(Y, X, reset, clk);
 
-input 			clk, reset; 
-input 	[7:0] 	X;
-output 	[9:0] 	Y;
-reg		[71:0] 	ValX;
-reg		[11:0]	r_sumX;
-wire	[7:0]	appr_out;
-wire 	[11:0] 	sumX;
-wire	[7:0] 	avgX;
-wire  	[9:0]	Y;
+    input clk, reset; 
+    input 	[7:0] X;
 
-parameter z = 0;
+    reg     [71:0] ValX;
+    reg     [11:0]   sum;
+    reg     [7:0]  Xavg;
+    reg     [7:0]   Xapp;
+    reg     [7:0]   Xapp_temporary;
+    reg     [7:0]   Xapp_compare1;
+    reg     [7:0]   Xapp_compare2;
+    output 	[9:0] Y;
+    reg 	[9:0] Y;
+    reg     [71:0] A;
+    integer counter;
+    integer i;
+    always @(posedge clk) begin
+        if (reset == 1) begin
+            Xavg <= 0;
+            Xapp <= 0;
+            ValX  <= 0;
+            sum <=0;
+            counter <= 0;
+            Y <= 0;
+            Xapp_compare1 <= 0;
+            Xapp_compare2 <= 0;
+        end
+        else begin
+            
+            if (counter < 8) begin
+                ValX = ValX << 8;
+                ValX[7:0] = X[7:0];
+                sum = sum + X;
+                
+                
+            end
+            else begin
+                sum = sum + X[7:0] - ValX[71:64];
+                ValX = ValX << 8;
+                ValX[7:0] = X[7:0];
+                
+                Xavg = sum/9;
+                Xapp = 0;
+                for(i=0; i<9; i = i+1) begin
+                    A = ValX >> 8*i;
+                    Xapp_temporary[7:0] = A[7:0];
+                    if (Xavg >= Xapp_temporary) 
+                        Xapp_compare1 = Xavg  - Xapp_temporary;
+                        Xapp_compare2 = Xavg  - Xapp;
+                        Xapp = (Xapp_compare1 < Xapp_compare2) ? Xapp_temporary:Xapp;
+                    
+                end
 
-//--------------------------------------
-//  \^o^/   Write your code here~  \^o^/
-//--------------------------------------
-always @(posedge clk or posedge reset) begin
-	if(reset) begin
-		ValX <= 72'b0;
-		r_sumX <= 8'b0;
+                Y = (sum + 9*Xapp)/8;
+                
+            end
+            counter = counter + 1;
 
-	end
-	else begin
-		ValX <= {ValX[63:0],X};
-		r_sumX <= sumX;
-	end	
-end
+        end
+    end
 
-assign sumX = r_sumX - {4'b0, ValX[71:64]} + {4'b0, X};
-assign avgX = r_sumX/9;
-
-Calculation cal(avgX,ValX,appr_out);
-
-assign Y = (appr_out*9 + r_sumX)/8;
-endmodule
-
-/* ################ */
-module Calculation(id_avgX,id_ValX,id_appr_out);
-
-input	[71:0]	id_ValX;
-input	[7:0]	id_avgX;
-output 	[7:0]	id_appr_out;
-
-reg		[7:0]	id_appr_out;
-reg 	[7:0]	appr_cal1[0:8];
-reg 	[7:0]	appr_cal2[0:8];
-reg		[71:0]	ValShiftX[0:8];
-reg		[7:0]	id_tranfer[0:8];
-integer i;
-
-always @(id_ValX,id_avgX) begin
-	id_appr_out = 8'b0;
-	for(i=0;i<9;i=i+1) begin
-	
-		case(i) 
-			0: begin
-				id_tranfer[i] = id_ValX[7:0];
-			end
-			1: begin
-				id_tranfer[i] = id_ValX[15:8];
-			end
-			2: begin
-				id_tranfer[i] = id_ValX[23:16];
-			end
-			3: begin
-				id_tranfer[i] = id_ValX[31:24];
-			end
-			4: begin
-				id_tranfer[i] = id_ValX[39:32];
-			end
-			5: begin
-				id_tranfer[i] = id_ValX[47:40];
-			end
-			6: begin
-				id_tranfer[i] = id_ValX[55:48];
-			end
-			7: begin
-				id_tranfer[i] = id_ValX[63:56];
-			end
-			8: begin
-				id_tranfer[i] = id_ValX[71:64];
-			end
-		
-		endcase
-		if (id_tranfer[i] <= id_avgX)  begin
-			appr_cal1[i] = id_avgX - id_tranfer[i];
-			appr_cal2[i] = id_avgX - id_appr_out;
-			if(appr_cal1[i][7:0] < appr_cal2[i][7:0]) begin
-				id_appr_out[7:0] = id_tranfer[i];
-			end
-		end
-		else begin
-			appr_cal1[i][7:0] = 0;
-			appr_cal2[i][7:0] = 0;
-		end
-	end	
-end
 endmodule
